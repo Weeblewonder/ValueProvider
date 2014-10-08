762cfc9d346dfcc06301b5265263999dbf06276b
Shaun Walker <shaun@nibbletech.co.uk>
2014-10-08 18:25:33 +0100

    Add fields setting

--- src/ValueProvider.php
+++ src/ValueProvider.php
@@ -7,13 +7,26 @@ class ValueProvider {
 
 	protected $valueClass;
 
-	function __construct($valueClass) {
+	function __construct($valueClass, array $fields = []) {
 		if(is_string($valueClass)){
 			$this->valueClass = $valueClass;
 		}else(is_object($valueClass)){
 			$this->valueClass = get_class($valueClass);
 		}else{
 			throw new \Exception("\$valueClass must be a string or object.", 1);
+		}
+
+		if( !empty($fields) ){
+			$this->setFields($fields);
+		}
+	}
+
+	public function setFields(array $fields, $merge = false)
+	{
+		if( $merge ){
+			$this->fields = array_merge($this->fields, $fields);
+		}else{
+			$this->fields = $fields;
 		}
 	}
 
@@ -39,4 +52,6 @@ class ValueProvider {
 			}
 		}
 	}
+
+
 }
